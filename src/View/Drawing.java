1138f84f3fc6f7f40a9df5cee5f3bfe5b0d79c15
Xiaxing SHI <starsasumi@gmail.com>
2015-10-22 23:28:10 +0200

    renaming folder names (folder names is case insensitive...)

--- src/View/Drawing.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-public class Drawing {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/view1/Drawing.java
@@ -0,0 +1,8 @@
+package view;
+
+public class Drawing {
+
+    public static void main(String[] args) {
+
+    }
+}
