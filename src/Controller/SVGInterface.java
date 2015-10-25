3bc13f1e5eb165d8a7da391fd3c3401b23c5c131
anqitong <none@none.com>
2015-10-25 16:52:31 +0100

    Modified SVG Interface

---
+++ src/Controller/SVGInterface.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.util.List;
+import model.Shapes;
+
+public interface SVGInterface {
+			
+		public boolean writeSVGFile(String content);
+		
+		//add shapes to a svg file
+		public boolean addShapes(List<Shapes> listShapes);
+		
+		public boolean display();	
+}
