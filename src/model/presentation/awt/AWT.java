1c0ad63f60e65d5d3d48521901b8b9f7e147cfe3
Xiaxing SHI <starsasumi@gmail.com>
2015-11-09 22:32:27 +0100

    Create AWT that implements Presentation

---
+++ src/model/presentation/awt/AWT.java
@@ -0,0 +1,7 @@
+package model.presentation.awt;
+
+/**
+ * Created by Xiaxing SHI on 09/11/15.
+ */
+public class AWT {
+}
---
+++ src/view/DrawAwt.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by Xiaxing SHI on 09/11/15.
+ */
+public class DrawAwt {
+}
